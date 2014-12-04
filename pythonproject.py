beab8af230a050b1971e66a6c7199562a2d1c5b5
Hannah-Pierre <s581460@student.roundrockisd.org>
2014-12-04 15:00:51 -0600

    Comments added

--- pythonproject.py
+++ pythonproject.py
@@ -6,6 +6,7 @@ targetx1 = 200
 targety1 = 40
 targetx2 = 280
 targety2 = 60
+#enemies 
 target = drawpad.create_rectangle(targetx1,targety1,targetx2,targety2, fill="red")
 enemy2 = drawpad.create_rectangle(200, 100, 280, 120, fill="red")
 enemy3 = drawpad.create_rectangle(200, 160, 280, 180, fill="red")
@@ -16,12 +17,11 @@ enemy7 = drawpad.create_rectangle(200, 400, 280, 420, fill="red")
 enemy8 = drawpad.create_rectangle(200, 460, 280, 480, fill="red")
 enemy9 = drawpad.create_rectangle(200, 520, 280, 540, fill="red")
 startarea = drawpad.create_rectangle(300,0,320,20, fill="white")
-#finished pattern
+#start area pattern
 drawpad.create_rectangle(300,0,305,5, fill ="black")
 drawpad.create_rectangle(310,0,315,5, fill ="black")
 drawpad.create_rectangle(305,5,310,10, fill ="black")
 drawpad.create_rectangle(315,5,320,10, fill ="black")
-#unfinished pattern
 drawpad.create_rectangle(300,10,305,15, fill ="black")
 drawpad.create_rectangle(310,10,315,15, fill ="black")
 drawpad.create_rectangle(305,15,310,20, fill ="black")
@@ -30,7 +30,7 @@ drawpad.create_rectangle(315,15,320,20, fill ="black")
 
 
 player = drawpad.create_oval(300,0,320,20, fill="magenta")
-
+#directions for enemies 
 direction = -1
 direction2 = 2 
 direction3 = -3
@@ -40,6 +40,7 @@ direction6 = 6
 direction7 = -7
 direction8 = 8
 direction9 = -9
+#go button control
 gopressed = False
 
 #creating buttons to move left and right 
@@ -52,7 +53,7 @@ class MyApp:
 		self.myContainer1.pack()
 		
 
-		
+		#making the buttons
 		self.button5 = Button(self.myContainer1)
 		self.button5.configure(text="Left", background= "pink")
 		self.button5.grid(row=2,column=0)
@@ -66,7 +67,7 @@ class MyApp:
 		self.gobutton.grid(row=1,column=2)
 
 					
-		# "Bind" an action to the first button												
+		# binding the buttons												
 
                 self.button5.bind("<Button-1>", self.button5Click)
 		self.button2.bind("<Button-1>", self.button2Click)
@@ -75,15 +76,16 @@ class MyApp:
 		
 		
 		  
-		# This creates the drawpad - no need to change this 
+		#creates the drawpad 
 		drawpad.pack()
 		self.animate()
+	#go button 
 
 	def gobuttonclick(self,event) :
 	       global gopressed
 	       gopressed = True
 	       
-	           
+	# left and right controls           
         def button2Click(self, event):  
                 global oval
 		global drawpad
@@ -99,6 +101,7 @@ class MyApp:
 		    drawpad.move(player, -20, 0)
 
 	def animate(self):
+	    #enemies animation
             global target
             global direction
             global gopressed
@@ -181,7 +184,7 @@ class MyApp:
                 direction9 = 9
             drawpad.move(enemy9,direction9,0)
             
-            
+            #player animation
             global player 
             x1,y1,x2,y2 = drawpad.coords(player)
             if gopressed == True :
@@ -190,63 +193,63 @@ class MyApp:
                 drawpad.delete(player)
             sx1, sy1, sx2, sy2 = drawpad.coords(startarea)
             
-            # enemy1
+            # enemy1 collision detect
             if y2 > targety1 and y2 < targety2 and x1 > targetx1 and x2 < targetx2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < targetx2 and x1 > targetx1 and y2 > targety1 and y2 < targety2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > targetx1 and x2 < targetx2 and y2 > targety1 and  y2 < targety2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy2    
+            #enemy2  collision detect  
             if y2 > e2y1 and y2 < e2y2 and x1 > e2x1 and x2 < e2x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e2x2 and x1 > e2x1 and y2 > e2y1 and y2 < e2y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e2x1 and x2 < e2x2 and y2 > e2y1 and y2 < e2y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy3    
+            #enemy3  collision detect  
             if y2 > e3y1 and y2 < e3y2 and x1 > e3x1 and x2 < e3x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e3x2 and x1 > e3x1 and y2 > e3y1 and y2 < e3y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e3x1 and x2 < e3x2 and y2 > e3y1 and y2 < e3y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy4
+            #enemy4 collision detect
             if y2 > e4y1 and y2 < e4y2 and x1 > e4x1 and x2 < e4x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e4x2 and x1 > e4x1 and y2 > e4y1 and y2 < e4y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e4x1 and x2 < e4x2 and y2 > e4y1 and y2 < e4y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy5    
+            #enemy5   collision detect 
             if y2 > e5y1 and y2 < e5y2 and x1 > e5x1 and x2 < e5x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e5x2 and x1 > e5x1 and y2 > e5y1 and y2 < e5y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e5x1 and x2 < e5x2 and y2 > e5y1 and y2 < e5y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2) 
-            #enemy6
+            #enemy6 collision detect
             if y2 > e6y1 and y2 < e6y2 and x1 > e6x1 and x2 < e6x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e6x2 and x1 > e6x1 and y2 > e6y1 and y2 < e6y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e6x1 and x2 < e6x2 and y2 > e6y1 and y2 < e6y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy7
+            #enemy7 collision detect
             if y2 > e7y1 and y2 < e7y2 and x1 > e7x1 and x2 < e7x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e7x2 and x1 > e7x1 and y2 > e7y1 and y2 < e7y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e7x1 and x2 < e7x2 and y2 > e7y1 and y2 < e7y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy8
+            #enemy8 collision detect
             if y2 > e8y1 and y2 < e8y2 and x1 > e8x1 and x2 < e8x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e8x2 and x1 > e8x1 and y2 > e8y1 and y2 < e8y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x2 > e8x1 and x2 < e8x2 and y2 > e8y1 and y2 < e8y2:
                 drawpad.move(player, sx1 - x1, sy2 - y2)
-            #enemy9 
+            #enemy9 collision detect
             if y2 > e9y1 and y2 < e9y2 and x1 > e9x1 and x2 < e9x2 :
                 drawpad.move(player, sx1 - x1, sy2 - y2)
             if x1 < e9x2 and x1 > e9x1 and y2 > e9y1 and y2 < e9y2:
@@ -266,8 +269,6 @@ class MyApp:
                     
                 
 
-direction = 1
-direction2 = 2
 
 
                
